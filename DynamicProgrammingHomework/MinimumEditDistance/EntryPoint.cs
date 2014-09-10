a81b6a5f107e2d24c17f997e887494e86b0f2a2f
Dimitar Kostov <d.kostov.dev@gmail.com>
2014-09-10 11:00:56 +0300

    Dynamic Programming - Task 2

--- DynamicProgrammingHomework/MinimumEditDistance/EntryPoint.cs
+++ DynamicProgrammingHomework/MinimumEditDistance/EntryPoint.cs
@@ -1,7 +1,6 @@
 ﻿namespace MinimumEditDistance
 {
     using System;
-    using System.Collections.Generic;
 
     /// <summary>
     /// Write a program to calculate the "Minimum Edit Distance" (MED) between two words. 
@@ -15,14 +14,17 @@
     /// delete ‘d’: "developer" - "eveloper", cost = 0.9
     /// insert ‘n’: "eveloper" - "enveloper", cost = 0.8
     /// replace ‘r’ - ‘d’: "enveloper" - "enveloped", cost = 1
-
     /// </summary>
     public class EntryPoint
     {
+        private const double DeleteCost = 0.9;
+        private const double InsertCost = 0.8;
+        private const double ReplaceCost = 1;
+
         public static void Main()
         {
-            char[] wordX = "aaab".ToCharArray();
-            char[] wordY = "bbba".ToCharArray();
+            char[] wordX = "developer".ToCharArray();
+            char[] wordY = "enveloped".ToCharArray();
             double result = EditDistanceDP(wordX, wordY);
             Console.WriteLine(result);
         }
@@ -38,53 +40,61 @@
 
             double[,] solutionTable = new double[rows, cols];
 
+            // Initial values
             for (int row = 0; row < rows; row++)
             {
-                for (int col = 0; col < cols; col++)
-                {
-                    solutionTable[row, col] = 0;
-                }
-            }
-
-            for (int row = 0; row < rows; row++)
-            {
-                solutionTable[row, 0] = row;
+                solutionTable[row, 0] = row * DeleteCost;
             }
 
             for (int col = 0; col < cols; col++)
             {
-                solutionTable[0, col] = col;
+                solutionTable[0, col] = col * DeleteCost;
             }
 
+            // Calculation
             for (int i = 1; i < rows; i++)
             {
                 for (int j = 1; j < cols; j++)
                 {
                     leftCell = solutionTable[i - 1, j];
-                    leftCell += 0.9;
+                    leftCell += DeleteCost;
 
                     topCell = solutionTable[i, j - 1];
-                    topCell += 0.8;
+                    topCell += InsertCost;
 
                     cornerCell = solutionTable[i - 1, j - 1];
 
                     if (wordX[i - 1] != wordY[j - 1])
                     {
-                        cornerCell += 1;
+                        cornerCell += ReplaceCost;
                     }
 
                     solutionTable[i, j] = MinimumBetweenThree(leftCell, topCell, cornerCell);
-                    // PRINT MATRIX
+                    //// PrintMatrix(solutionTable);
                 }
             }
 
-            // TO DO: FIX BUG
             return solutionTable[rows - 1, cols - 1];
         }
 
         private static double MinimumBetweenThree(double a, double b, double c)
         {
             return Math.Min(Math.Min(a, b), c);
+        }
+
+        private static void PrintMatrix(double[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write("[{0}]", matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
         }
     }
 }
