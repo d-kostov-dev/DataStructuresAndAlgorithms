46a9d39f7a55df67e6582bc6ef8440b6774a10e4
d-kostov-dev <d.kostov.dev@gmail.com>
2014-09-10 03:15:32 +0300

    Dynamic Programming Homework - Task 1

---
+++ DynamicProgrammingHomework/KnapsackProblem/EntryPoint.cs
@@ -0,0 +1,129 @@
+﻿namespace KnapsackProblem
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    /// <summary>
+    /// Write a program based on dynamic programming to solve the "Knapsack Problem": 
+    /// you are given N products, 
+    /// each has weight Wi 
+    /// and costs Ci 
+    /// and a knapsack of capacity M and you want to put inside a subset of the products with highest 
+    /// cost and weight ≤ M. 
+    /// The numbers N, M, Wi and Ci are integers in the range [1..500]. 
+    /// </summary>
+    public class EntryPoint
+    {
+        private static int knapsackCapacity;
+        private static IList<Product> availableProducts;
+
+        public static void Main()
+        {
+            GetInputData();
+            Console.WriteLine();
+
+            var fittingProducts = FindBestProductsCombination();
+
+            Console.WriteLine("Best weight/value we can get is:");
+            Console.WriteLine(string.Join("\n", fittingProducts));
+        }
+
+        public static IList<Product> FindBestProductsCombination()
+        {
+            // Keep the best values in each capacity of the bag.
+            int[,] costMatrix = new int[availableProducts.Count + 1, knapsackCapacity + 1];
+
+            // Shoud we keep the product or not.
+            bool[,] doKeep = new bool[availableProducts.Count + 1, knapsackCapacity + 1];
+
+            // Loop through all products.
+            for (int productNumber = 1, len = availableProducts.Count; productNumber <= len; productNumber++) 
+            {
+                var currentProduct = availableProducts[productNumber - 1];
+
+                // Try each bag capacity options.
+                for (int currentCapacity = 1; currentCapacity <= knapsackCapacity; currentCapacity++) 
+                {
+                    if (currentProduct.Weight <= currentCapacity) 
+                    {
+                        int remainingCapacity = currentCapacity - currentProduct.Weight; 
+
+                        if (remainingCapacity > 0)
+                        {
+                            // The previous calculation for the same capacity.
+                            int previousCost = costMatrix[productNumber - 1, currentCapacity];
+
+                            // The previous calculation for the remaining capacity and previous product.
+                            int sumValue = 
+                                currentProduct.Cost + costMatrix[productNumber - 1, remainingCapacity - 1];
+
+                            // If previous cost is better we keep it and don't save the product.
+                            if (previousCost > sumValue) 
+                            {
+                                costMatrix[productNumber, currentCapacity] = previousCost;
+                                doKeep[productNumber, currentCapacity] = false;
+                            }
+                            else
+                            {
+                                costMatrix[productNumber, currentCapacity] = sumValue;
+                                doKeep[productNumber, currentCapacity] = true;
+                            }
+                        }
+                        else
+                        {
+                            // No previous calculations. Best case so far. Save the product.
+                            costMatrix[productNumber, currentCapacity] = currentProduct.Cost;
+                            doKeep[productNumber, currentCapacity] = true;
+                        }
+                    }
+                }
+            }
+
+           var bestCombination = new List<Product>();
+
+            int remainingCap = knapsackCapacity;
+            int product = availableProducts.Count;
+
+            // We get the first possible product. Decrease weight...get next possible.
+            while (product >= 0)
+            {
+                bool toBeAdded = doKeep[product, remainingCap - 1];
+
+                if (toBeAdded)
+                {
+                    bestCombination.Add(availableProducts[product - 1]);
+                    remainingCap -= availableProducts[product - 1].Weight;
+                }
+
+                product--;
+            }
+
+            return bestCombination;
+        }
+
+        private static void GetInputData()
+        {
+            Console.SetIn(new StreamReader("../../inputData.txt"));
+
+            Console.Write("Knapsack Capacity: ");
+            knapsackCapacity = int.Parse(Console.ReadLine());
+
+            Console.Write("Numer of products: ");
+            int n = int.Parse(Console.ReadLine());
+            availableProducts = new List<Product>(n);
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter Product 'Name Weight Price'");
+                var currentInput = Console.ReadLine().Split(' ').Select(s => s.Trim()).ToArray();
+
+                var currentProduct =
+                    new Product(currentInput[0], int.Parse(currentInput[1]), int.Parse(currentInput[2]));
+
+                availableProducts.Add(currentProduct);
+            }
+        }
+    }
+}
---
+++ DynamicProgrammingHomework/MinimumEditDistance/EntryPoint.cs
@@ -0,0 +1,90 @@
+﻿namespace MinimumEditDistance
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Write a program to calculate the "Minimum Edit Distance" (MED) between two words. 
+    /// MED(x, y) is the minimal sum of costs of edit operations used to transform x to y. 
+    /// Sample costs are given below:
+    ///  - cost (replace a letter) = 1
+    ///  - cost (delete a letter) = 0.9
+    ///  - cost (insert a letter) = 0.8
+    /// 
+    /// Example: x = "developer", y = "enveloped" - cost = 2.7 
+    /// delete ‘d’: "developer" - "eveloper", cost = 0.9
+    /// insert ‘n’: "eveloper" - "enveloper", cost = 0.8
+    /// replace ‘r’ - ‘d’: "enveloper" - "enveloped", cost = 1
+
+    /// </summary>
+    public class EntryPoint
+    {
+        public static void Main()
+        {
+            char[] wordX = "aaab".ToCharArray();
+            char[] wordY = "bbba".ToCharArray();
+            double result = EditDistanceDP(wordX, wordY);
+            Console.WriteLine(result);
+        }
+
+        private static double EditDistanceDP(char[] wordX, char[] wordY)
+        {
+            double leftCell = 0;
+            double topCell = 0;
+            double cornerCell = 0;
+
+            int rows = wordX.Length + 1;
+            int cols = wordY.Length + 1;
+
+            double[,] solutionTable = new double[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    solutionTable[row, col] = 0;
+                }
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                solutionTable[row, 0] = row;
+            }
+
+            for (int col = 0; col < cols; col++)
+            {
+                solutionTable[0, col] = col;
+            }
+
+            for (int i = 1; i < rows; i++)
+            {
+                for (int j = 1; j < cols; j++)
+                {
+                    leftCell = solutionTable[i - 1, j];
+                    leftCell += 0.9;
+
+                    topCell = solutionTable[i, j - 1];
+                    topCell += 0.8;
+
+                    cornerCell = solutionTable[i - 1, j - 1];
+
+                    if (wordX[i - 1] != wordY[j - 1])
+                    {
+                        cornerCell += 1;
+                    }
+
+                    solutionTable[i, j] = MinimumBetweenThree(leftCell, topCell, cornerCell);
+                    // PRINT MATRIX
+                }
+            }
+
+            // TO DO: FIX BUG
+            return solutionTable[rows - 1, cols - 1];
+        }
+
+        private static double MinimumBetweenThree(double a, double b, double c)
+        {
+            return Math.Min(Math.Min(a, b), c);
+        }
+    }
+}
